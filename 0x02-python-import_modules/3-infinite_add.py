10c4cb8d8a4e668f13ebe2711d41e2a079d3dcdc
Naola1 <naolmitiku@gmail.com>
2022-09-22 22:24:55 +0300

    task3 commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     a = len(sys.argv)
     sum = 0
     for i in range(1, a):
-	    sum = sum + int(sys.argv[i])
+        sum = sum + int(sys.argv[i])
     print('{:d}'.format(sum))
