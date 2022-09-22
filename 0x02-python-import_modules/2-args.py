997328a03e6bbae47c26d5c2739bf1e938c2ec67
Naola1 <naolmitiku@gmail.com>
2022-09-22 21:32:41 +0300

    task2 commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 import sys
 
 a = int(len(sys.argv))
-print(f'{a - 1} arguments')
+print(f'{a - 1} arguments:')
 for i in range(1, a):
-    print(f'{i} : {sys.argv[i]}')
+    print(f'{i}: {sys.argv[i]}')
