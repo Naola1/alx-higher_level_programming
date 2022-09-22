fd5d3c0606a04de3ec3a867e3adbc3220f80f327
Naola1 <naolmitiku@gmail.com>
2022-09-22 22:37:04 +0300

    task4 commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,9 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    from hidden_4 import* 
+    import hidden_4
 
     names = dir(hidden_4)
     for name in names:
+        if name[:2] != "__":
             print(name)
