c334c4c832b024f5c4c8426e2000c616bed1a24d
Naola1 <naolmitiku@gmail.com>
2022-09-22 21:34:24 +0300

    task2 commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-a = int(len(sys.argv))
-print(f'{a - 1} arguments:')
-for i in range(1, a):
-    print(f'{i}: {sys.argv[i]}')
+    a = int(len(sys.argv))
+    print(f'{a - 1} arguments:')
+    for i in range(1, a):
+        print(f'{i}: {sys.argv[i]}')
