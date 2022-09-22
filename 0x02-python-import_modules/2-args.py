7c34ce134e8ad33f79ff534751d6fd7e11f9a237
Naola1 <naolmitiku@gmail.com>
2022-09-22 21:38:15 +0300

    task2 commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     import sys
 
     a = int(len(sys.argv))
-    print(f'{a - 1} arguments:')
-    for i in range(1, a):
-        print(f'{i}: {sys.argv[i]}')
+    if a == 0:
+        print('0 arguments.')
+    else:
+        print(f'{a - 1} arguments:')
+        for i in range(1, a):
+            print(f'{i}: {sys.argv[i]}')
