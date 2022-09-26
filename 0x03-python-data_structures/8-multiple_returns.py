2b3cbbe79df8f6fbb5c0f83394efa744a32f38f2
Naola1 <naolmitiku@gmail.com>
2022-09-26 14:10:28 +0300

    task8 commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return length, first
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
