ff9a87a0ba4764d615e44f1fa31822d2e4461afc
Naola1 <naolmitiku@gmail.com>
2022-09-30 21:40:01 +0300

    task9 commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-       for keys in sorted(a_dictionary.keys()):
-        print('{}: {}'. format(keys, a_dictionary[keys]*2))
+    return {key: val * 2 for key, val in a_dictionary.items()}
