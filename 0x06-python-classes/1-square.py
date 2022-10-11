d3b28b216379b17f2af1aa24e256a3a3d5cb7c2e
Naola1 <naolmitiku@gmail.com>
2022-10-11 19:57:24 +0300

    task1 commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,6 +8,7 @@ class Square:
 
     def __init__(self, size):
     '''Initializing this square class
-    Args: size - represents the size of the square defined'''
+    Args: size - represents the size of the square defined
+    '''
 
         self.__size = size
