6aa5b51e18ab654265dd3a649a5abff764ed21df
Naola1 <naolmitiku@gmail.com>
2022-10-17 18:42:33 +0300

    task0 commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines Rectangle"""
+"""A class that defines a rectangle"""
+
 
 class Rectangle:
-
-    """Does nothing"""
+    """An empty class that represents a rectangle"""
     pass
